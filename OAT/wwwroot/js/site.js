ce0a4140e816b3acc2d7402837d2e7e670a9b16b
All-exei <aleksandrpehterev26@gmail.com>
2023-05-24 00:03:09 +0600

    first steps: beta version of header

--- OAT/wwwroot/js/site.js
+++ OAT/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+'use strict'
+window.addEventListener("load", () => {
+    let header = document.getElementById("site-header");
+    let menuIcon = document.getElementById("menu-icon");
+
+    menuIcon.addEventListener("click", () => {
+        header.classList.toggle("header-drop");
+    });
+});
