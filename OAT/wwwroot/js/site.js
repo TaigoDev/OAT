5df32a0f279570445644c1bcf573a2631f278d8d
All-exei <aleksandrpehterev26@gmail.com>
2023-05-26 20:38:10 +0600

    beta version of header

--- OAT/wwwroot/js/site.js
+++ OAT/wwwroot/js/site.js
@@ -7,6 +7,14 @@
 window.addEventListener("load", () => {
     let header = document.getElementById("site-header");
     let menuIcon = document.getElementById("menu-icon");
+    let dropdownMenuElements = document.querySelectorAll(".dropdown-menu-item");
+
+    dropdownMenuElements.forEach(element => {
+        element.querySelector(".dropdown-menu-item-title").addEventListener("click", () => {
+            dropdownMenuElements.forEach(element => element.classList.add("dropdown-menu-item-inactive"));
+            element.classList.toggle("dropdown-menu-item-inactive");
+        });
+    });
 
     menuIcon.addEventListener("click", () => {
         header.classList.toggle("header-drop");
