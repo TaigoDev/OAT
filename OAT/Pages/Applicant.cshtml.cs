686608a6d19357a53957b5448b8a0db4e03b2274
All-exei <aleksandrpehterev26@gmail.com>
2023-05-31 16:08:20 +0600

    add applicant page + little visual fixes at main page

---
+++ OAT/Pages/Applicant.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OAT.Pages
+{
+	public class ApplicantModel : PageModel
+	{
+		private readonly ILogger<IndexModel> _logger;
+
+		public ApplicantModel(ILogger<IndexModel> logger)
+		{
+			_logger = logger;
+		}
+
+		public void OnGet()
+		{
+
+		}
+	}
+}
