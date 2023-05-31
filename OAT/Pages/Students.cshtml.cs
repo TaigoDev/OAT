1d8264d6a6b1f71f6d0136dec937cff3c0916e10
All-exei <aleksandrpehterev26@gmail.com>
2023-05-31 21:55:52 +0600

    add first not main page

---
+++ OAT/Pages/Students.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OAT.Pages
+{
+	public class StudentsModel : PageModel
+	{
+		private readonly ILogger<IndexModel> _logger;
+
+		public StudentsModel(ILogger<IndexModel> logger)
+		{
+			_logger = logger;
+		}
+
+		public void OnGet()
+		{
+
+		}
+	}
+}
