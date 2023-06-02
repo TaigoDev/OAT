105858fbec90505273ddd6fd7e610d8dd99c3e6a
All-exei <aleksandrpehterev26@gmail.com>
2023-06-03 00:57:30 +0600

    alpha version of accordion

---
+++ OAT/Pages/organizationInformation/CommonIntelligence.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OAT.Pages.organizationInformation
+{
+    public class CommonIntelligenceModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public CommonIntelligenceModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
