1bef19ac0092bfe6e55ddc9e8f4f7e3e9942a490
All-exei <aleksandrpehterev26@gmail.com>
2023-06-11 16:48:10 +0600

    add internationalCooperation page + add images slider + change feedback card visual + add another people at pedagogical staff

---
+++ OAT/Pages/organizationInformation/InternationalCooperation.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OAT.Pages.organizationInformation
+{
+    public class InternationalCooperationModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public InternationalCooperationModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
--- OAT/wwwroot/js/site.js
+++ OAT/wwwroot/js/site.js
@@ -5,13 +5,17 @@
 
 'use strict'
 window.addEventListener("load", () => {
+    /* main elements */
     let mainMenuWindow = document.getElementById("main-menu-window");
     let mainMenu = document.getElementById("main-menu");
     let menuIcon = document.getElementById("menu-icon");
     let mainMenuElements = document.querySelectorAll(".main-menu-item");
+
+    /* optional elements */
     let accordionsList = document.querySelectorAll(".accordion");
+    let imagesSliders = document.querySelectorAll(".images-list-slider-container");
 
-
+    /* main menu functional */
     mainMenuElements.forEach(element => {
         element.querySelector(".main-menu-item-title").addEventListener("click", () => {
             mainMenuElements.forEach(element => element.classList.add("main-menu-item-inactive"));
@@ -30,6 +34,7 @@ window.addEventListener("load", () => {
         mainMenuWindow.classList.toggle("main-menu-window-open");
     });
 
+    /* accordion functional */
     accordionsList.forEach(accordion => {
         accordion.querySelector(".accordion-head").insertAdjacentHTML("beforeend", `<img id="eye-icon" class="icon accordion-head-icon" src="/images/basic/accordionMore.svg" alt="версия для слабовидящих">`);
         console.log(accordion.querySelector(".accordion-head-icon"));
@@ -40,4 +45,41 @@ window.addEventListener("load", () => {
             accordion.classList.add("open");
         });
     });
+
+    /* images slider functional */
+    for (let i = 0; i < imagesSliders.length; i++) {
+        let sliderContainer = imagesSliders[i];
+        sliderContainer.insertAdjacentHTML("afterbegin", `<div class="images-list-slider-container-btn prev"></div><div class="images-list-slider-container-btn next">
+            </div>`);
+        let sliderContainerPrev = sliderContainer.querySelector(".prev");
+        let sliderContainerNext = sliderContainer.querySelector(".next");
+        sliderContainerPrev.classList.add("images-list-slider-container-btn-inactive");
+
+        let slider = sliderContainer.querySelector(".images-list-slider");
+        let countOfImages = slider.querySelectorAll("img").length;
+        let currentImg = 0;
+        console.log(countOfImages);
+
+        sliderContainerPrev.addEventListener("click", () => {
+            if (currentImg > 0) {
+                currentImg--;
+                slider.style = `--images-slider-helper: ${currentImg};`;
+                sliderContainerNext.classList.remove("images-list-slider-container-btn-inactive");
+            }
+            if (currentImg == 0) {
+                sliderContainerPrev.classList.add("images-list-slider-container-btn-inactive");
+            }
+        });
+
+        sliderContainerNext.addEventListener("click", () => {
+            if (currentImg < countOfImages - 1) {
+                currentImg++;
+                slider.style = `--images-slider-helper: ${currentImg};`;
+                sliderContainerPrev.classList.remove("images-list-slider-container-btn-inactive");
+            }
+            if (currentImg == countOfImages - 1) {
+                sliderContainerNext.classList.add("images-list-slider-container-btn-inactive");
+            }
+        });
+    }
 });
