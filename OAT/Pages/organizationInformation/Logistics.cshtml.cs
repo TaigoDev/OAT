02735fdf24c7d7c6f3c7e6963d5e0f38feb4e5df
All-exei <aleksandrpehterev26@gmail.com>
2023-06-12 13:14:33 +0600

    rebuild commonIntelligence page + add Logistics page (alpha) + bug fixes

---
+++ OAT/Pages/organizationInformation/Logistics.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OAT.Pages.organizationInformation
+{
+    public class LogisticsModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public LogisticsModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
