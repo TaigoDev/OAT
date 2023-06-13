b18025d6dbb5cc8c6a6bd8df7aba51ec3cf1a872
All-exei <aleksandrpehterev26@gmail.com>
2023-06-13 19:15:29 +0600

    add StudentSupport page (beta) + add folders for documents groups + add opacity to main menu icon

---
+++ OAT/Pages/organizationInformation/StudentSupport.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OAT.Pages.organizationInformation
+{
+    public class StudentSupportModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public StudentSupportModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
