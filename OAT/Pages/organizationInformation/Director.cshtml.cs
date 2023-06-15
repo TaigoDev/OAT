e3fbf9c700e8716e5dc60f745866bc8f333bb741
All-exei <aleksandrpehterev26@gmail.com>
2023-06-15 22:45:55 +0600

    design fixes + add staff navigation + add alpha financeEconomicActivity page + add alpha page of Director

---
+++ OAT/Pages/organizationInformation/Director.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OAT.Pages.organizationInformation
+{
+    public class DirectorModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public DirectorModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
