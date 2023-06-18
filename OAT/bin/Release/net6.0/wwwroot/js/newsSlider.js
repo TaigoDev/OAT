0c38905780dfd86138ede26792c08eab11fd7aed
Таисия Носкова <cyberchopa@gmail.com>
2023-06-18 18:09:20 +0600

    Upload new version on panel

---
+++ OAT/bin/Release/net6.0/wwwroot/js/newsSlider.js
@@ -0,0 +1,24 @@
+'use strict'
+window.addEventListener("load", () => {
+    let newsSlider = document.getElementById("news-slider");
+    let sliderPrev = document.getElementById("slider-prev");
+    let sliderNext = document.getElementById("slider-next");
+
+    let slidePosition = 0;
+    sliderPrev.addEventListener("click", () => {
+        if (slidePosition == 1) {
+            slidePosition--;
+            newsSlider.style = `--news-offset: ${-100 * slidePosition}%; transform: translateX(var(--news-offset));`;
+        }
+        if (slidePosition > 1) {
+            slidePosition--;
+            newsSlider.style = `--news-offset: ${-100 * slidePosition}%; transform: translateX(calc(var(--news-offset) - var(--news-offset-helper) * ${slidePosition}));`;
+        }
+    });
+    sliderNext.addEventListener("click", () => {
+        if (slidePosition < 5) {
+            slidePosition++;
+            newsSlider.style = `--news-offset: ${-100 * slidePosition}%; transform: translateX(calc(var(--news-offset) - var(--news-offset-helper) * ${slidePosition}));`;
+        }
+    });
+});
