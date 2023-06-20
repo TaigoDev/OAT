567cf20cb83d1239243d4a2998952d69d4737323
All-exei <aleksandrpehterev26@gmail.com>
2023-06-21 00:19:38 +0600

    main menu bug fix

--- OAT/Program.cs
+++ OAT/Program.cs
@@ -4,20 +4,8 @@ builder.Services.AddRazorPages();
 builder.WebHost.UseUrls("http://0.0.0.0:20045");
 
 var app = builder.Build();
-
-if (!app.Environment.IsDevelopment())
-{
-	app.UseExceptionHandler("/Error");
-	app.UseHsts();
-}
-
-app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapRazorPages();
-
 app.Run();
