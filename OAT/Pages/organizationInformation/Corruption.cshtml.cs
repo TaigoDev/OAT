38a18f0fddcfeb57f508fcb2b732261af69465dc
All-exei <aleksandrpehterev26@gmail.com>
2023-06-24 23:17:11 +0600

    added some documents on the education page + add corruption page

---
+++ OAT/Pages/organizationInformation/Corruption.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OAT.Pages.organizationInformation
+{
+    public class CorruptionModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public CorruptionModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
