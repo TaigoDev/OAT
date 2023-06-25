6035d17c7e912ff42db150e3120aea281c0b600b
All-exei <aleksandrpehterev26@gmail.com>
2023-06-25 18:44:55 +0600

    add panel page + add vacancies page

---
+++ OAT/Pages/organizationInformation/Vacancies.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OAT.Pages.organizationInformation
+{
+    public class VacanciesModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public VacanciesModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ OAT/Pages/panel/Panel.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OAT.Pages
+{
+	public class PanelModel : PageModel
+	{
+		private readonly ILogger<IndexModel> _logger;
+
+		public PanelModel(ILogger<IndexModel> logger)
+		{
+			_logger = logger;
+		}
+
+		public void OnGet()
+		{
+
+		}
+	}
+}
