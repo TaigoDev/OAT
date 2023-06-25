ad1fd55a17666d7b0f8e372c0b86fa69b02759e4
Таисия Носкова <cyberchopa@gmail.com>
2023-06-25 18:55:56 +0600

    123

--- OAT/Program.cs
+++ OAT/Program.cs
@@ -1,8 +1,10 @@
+using OAT.function;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
 builder.WebHost.UseUrls("http://0.0.0.0:20045");
-
+NewsController.init();
 var app = builder.Build();
 app.UseStaticFiles();
 app.UseRouting();
