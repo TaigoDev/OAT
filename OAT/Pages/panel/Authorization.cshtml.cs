ba52d118e36d3da38b5f3fefc36a425b569b02fe
All-exei <aleksandrpehterev26@gmail.com>
2023-06-25 20:10:25 +0600

    add panel alpha version with bugs + add authorization page

---
+++ OAT/Pages/panel/Authorization.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OAT.Pages
+{
+	public class AuthorizationModel : PageModel
+	{
+		private readonly ILogger<IndexModel> _logger;
+
+		public AuthorizationModel(ILogger<IndexModel> logger)
+		{
+			_logger = logger;
+		}
+
+		public void OnGet()
+		{
+
+		}
+	}
+}
