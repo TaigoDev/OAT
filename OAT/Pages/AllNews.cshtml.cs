063684305c8bf0c76cbb4c81ecb8079bde16e0a8
All-exei <aleksandrpehterev26@gmail.com>
2023-06-25 21:22:42 +0600

    add news page + add allNews page

---
+++ OAT/Pages/AllNews.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OAT.Pages
+{
+	public class AllNewsModel : PageModel
+	{
+		private readonly ILogger<IndexModel> _logger;
+
+		public AllNewsModel(ILogger<IndexModel> logger)
+		{
+			_logger = logger;
+		}
+
+		public void OnGet()
+		{
+
+		}
+	}
+}
