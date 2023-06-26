1f4160f7900455bda3d65bf102622777b2d79265
All-exei <aleksandrpehterev26@gmail.com>
2023-06-26 18:57:38 +0600

    add library page + add vacancies page + add jobs page

---
+++ OAT/Pages/organizationInformation/Jobs.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OAT.Pages.organizationInformation
+{
+    public class JobsModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public JobsModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ OAT/Pages/organizationInformation/Library.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OAT.Pages.organizationInformation
+{
+    public class LibraryModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public LibraryModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
