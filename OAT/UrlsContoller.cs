de1e6589324322d8352a2d230466be82dd5916d6
All-exei <aleksandrpehterev26@gmail.com>
2023-07-04 15:40:34 +0600

    add pages to UrlsController + bug fixes

--- OAT/UrlsContoller.cs
+++ OAT/UrlsContoller.cs
@@ -10,7 +10,7 @@ public class UrlsContoller
         if (cPage.Count() != 0)
             return cPage.FirstOrDefault()?.current;
 
-        var page = pages.Where(p => $"/{p}" == path || $"/{p}/" == path).FirstOrDefault();
+        var page = pages.Where(p => $"/{p.map}" == path || $"/{p.map}/" == path).FirstOrDefault();
         return page == null ? null : page.current;
     }
 
@@ -19,27 +19,46 @@ public class UrlsContoller
         var urls = new List<IPage>()
         {
             /*
-             * Если страница готова, то в первом аргументе прописываем новую относительную ссылку, 
-             * а во втором аргументе - относительную ссылку битриксу
+             * 1) Если страница готова, то в первом аргументе прописываем  относительную ссылку битрикса, 
+             * а во втором аргументе - новую относительную ссылку в ASP
              * 
-             * Если страница не готова, то в первом аргументе прописываем относительную ссылку битрикса со "/" в конце!,
-             * а во втором аргументе - новую относительную ссылку
+             * 2) Если страница не готова, то в первом аргументе прописываем новую относительную ссылку,
+             * а во втором аргументе - относительную ссылку битрикса со "/" в конце!
              * Во всех предыдущих случаях на других страницах мы указываем новую относительную ссылку
              * 
-             * Если файл страницы вообще отсутствует, то на других страницах, ссылающихся на данную, 
+             * 3) Если файл страницы вообще отсутствует, то на других страницах, ссылающихся на данную, 
              * мы указываем относительную ссылку на битрикс. В файле UrlsController ничего не указываем.
+             * ШАБЛОН: new IPage("ссылка, которую мы заменяем", "ссылка, на которую мы заменяем")
              */ 
+
             // main links
-            new IPage("allnews", "novosti"),
-            new IPage("applicant", "abitur"),
+            new IPage("novosti", "AllNews"),
+            new IPage("abitur", "Applicant"),
+            new IPage("students", "Students"),
             // organization information
-            new IPage("organizationInformation/AccessibleEnvironment", "sveden/ovz"),
-            new IPage("organizationInformation/CommonIntelligence", "sveden/common"),
-            new IPage("organizationInformation/Jobs", "sveden/vacant"),
-            new IPage("organizationInformation/Vacancies", "sveden/vacancy"),
+            new IPage("sveden/ovz", "organizationInformation/AccessibleEnvironment"),
+            new IPage("sveden/common", "organizationInformation/CommonIntelligence"),
+            new IPage("sveden/corup/index.php", "organizationInformation/Corruption"),
+            new IPage("sveden/document", "organizationInformation/Documents"),
+            new IPage("sveden/education", "organizationInformation/Education"),
+            new IPage("parents/organizatsiya-vospitatelnogo-protsessa-v-kolledzhe", "organizationInformation/EducationalProcess"),
+            new IPage("sveden/obrStandart.php", "organizationInformation/EducationalStandards"),
+            new IPage("sveden/budget", "organizationInformation/FinancialEconomicActivity"),
+            new IPage("sveden/inter", "organizationInformation/InternationalCooperation"),
+            new IPage("sveden/inter", "organizationInformation/InternationalCooperation"),
+            new IPage("sveden/vacant", "organizationInformation/Jobs"),
+            new IPage("sveden/biblioteka", "organizationInformation/Library"),
+            new IPage("sveden/objects", "organizationInformation/Logistics"),
+            new IPage("sveden/paid_edu", "organizationInformation/PaidEducationalServices"),
+            new IPage("sveden/struct", "organizationInformation/Structure"),
+            new IPage("sveden/grants", "organizationInformation/StudentSupport"),
+            new IPage("sveden/terror/index.php", "organizationInformation/Terrorism"),
+            new IPage("sveden/vacancy", "organizationInformation/Vacancies"),
+            // for Applicants
+
 
             // limitation
-            //new IPage("sveden/ovz/", "organizationInformation/AccessibleEnvironment"),
+            new IPage("organizationInformation/ManagementPedagogicalStaff", "sveden/employees/"),
         };
         pages = urls;
     }
