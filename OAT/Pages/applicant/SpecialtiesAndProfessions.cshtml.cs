2a73cabe719ae71dae970db4ba99909108fd3200
All-exei <aleksandrpehterev26@gmail.com>
2023-07-07 19:42:22 +0600

    add specialitiesAndProfessions page

---
+++ OAT/Pages/applicant/SpecialtiesAndProfessions.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OAT.Pages.applicant
+{
+    public class SpecialtiesAndProfessionsModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public SpecialtiesAndProfessionsModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
