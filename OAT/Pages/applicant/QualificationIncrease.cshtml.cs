b112dd676b08384e6775c7879a705f226efa9c7a
All-exei <aleksandrpehterev26@gmail.com>
2023-07-08 13:27:03 +0600

    add individualTraining page + add trainingCourses page + add QualificationIncrease page (beta) + add sitemap icon at header

---
+++ OAT/Pages/applicant/QualificationIncrease.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OAT.Pages.applicant
+{
+    public class QualificationIncreaseModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public QualificationIncreaseModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
