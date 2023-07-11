1de961e88ef47973a39546b0e978599581e69d55
All-exei <aleksandrpehterev26@gmail.com>
2023-07-11 12:00:13 +0600

    reworked all pages navigations at "organizationInformation" folder + add admissionRules page + add careerGuidanceEvents page + add DLinkCisco page + add specialitiesPartTimeLearning page + add dustanceFullTimeLearning page

---
+++ OAT/Pages/applicant/CareerGuidanceEvents.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OAT.Pages.applicant
+{
+    public class CareerGuidanceEventsModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public CareerGuidanceEventsModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
