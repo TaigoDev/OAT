2ab907bfa9268fc7605901bb57fe69b4cfcd98cb
All-exei <aleksandrpehterev26@gmail.com>
2023-07-12 12:37:35 +0600

    add alumniEmploymentCenterOmaviat page + add alumniEmploymentCenterOmsk page

---
+++ OAT/Pages/students/AlumniEmploymentCenterOmsk.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OAT.Pages.students
+{
+    public class AlumniEmploymentCenterOmskModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public AlumniEmploymentCenterOmskModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
