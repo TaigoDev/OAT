a4a7da7ef5e7fcc5f71e6621f046a6605e0f110c
All-exei <aleksandrpehterev26@gmail.com>
2023-07-14 12:31:15 +0600

    add timetable pages (without documents) + little change css

---
+++ OAT/Pages/timetable/TimetablePODPO.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OAT.Pages.timetable
+{
+    public class TimetablePODPOModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public TimetablePODPOModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ OAT/Pages/timetable/TimetableSession.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OAT.Pages.timetable
+{
+    public class TimetableSessionModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public TimetableSessionModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
