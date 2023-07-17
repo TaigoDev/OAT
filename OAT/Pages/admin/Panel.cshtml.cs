06957633cbe74d9a21049ce04410dc5e8db88c6d
TaigoStudio <studio@taigo.xyz>
2023-07-17 18:18:20 +0600

    New admin url

---
+++ OAT/Pages/admin/Panel.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OAT.Pages
+{
+    [Authorize]
+    public class PanelModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public PanelModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
