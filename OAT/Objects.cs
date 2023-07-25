de9f042c7bcfd09345523cc0e3588a6ed792c440
TaigoStudio <studio@taigo.xyz>
2023-07-25 20:51:46 +0600

    Schedule

--- OAT/Objects.cs
+++ OAT/Objects.cs
@@ -64,3 +64,68 @@ public class TelegramConfig
     public string token { get; set; }
     public string chat_id { get; set; }
 }
+public class Group
+{
+    public Group(string name, List<Week> weeks)
+    {
+        this.name = name;
+        this.weeks = weeks;
+    }
+
+    public string name { get; set; }
+    public List<Week> weeks { get; set; } = new List<Week>(2);
+}
+
+public class Week
+{
+    public Week(int id, List<Day> days)
+    {
+        this.id = id;
+        this.days = days;
+    }
+
+    public int id { get; set; }
+    public List<Day> days { get; set; } = new List<Day>(7);
+}
+
+public class Day
+{
+    public Day(string name, List<Lesson> lessons)
+    {
+        this.name = name;
+        this.lessons = lessons;
+    }
+
+    public string name { get; set; }
+    public List<Lesson> lessons { get; set; } = new List<Lesson>();
+}
+
+public class Lesson
+{
+    public Lesson(int id, List<Subgroup> subgroups)
+    {
+        this.id = id;
+        this.subgroups = subgroups;
+    }
+
+    public int id { get; set; }
+    public List<Subgroup> subgroups { get; set; } = new List<Subgroup>();
+}
+
+public class Subgroup
+{
+    public Subgroup(int id, string subject, string short_subject, string teacher, string cabinet)
+    {
+        this.id = id;
+        this.subject = subject;
+        this.short_subject = short_subject;
+        this.teacher = teacher;
+        this.cabinet = cabinet;
+    }
+
+    public int id { get; set; }
+    public string subject { get; set; }
+    public string short_subject { get; set; }
+    public string teacher { get; set; }
+    public string cabinet { get; set; }
+}
--- OAT/Readers/ScheduleReader.cs
+++ OAT/Readers/ScheduleReader.cs
@@ -1,27 +1,83 @@
-﻿using System.Xml;
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Xml;
+using static OAT.Readers.ScheduleReader;
 
 namespace OAT.Readers
 {
     public class ScheduleReader
     {
 
+        public static List<Group> groups = new List<Group>();
+
         public static void init()
         {
-            var xDoc = new XmlDocument();
-            var path = Path.Combine(Directory.GetCurrentDirectory(), "schedule.xml");
-            if(!File.Exists(path))
+            try
             {
-                Logger.Error("Файл schedule.xml не найден!");
-                return;
+                var xDoc = new XmlDocument();
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "schedule.xml");
+                if (!File.Exists(path))
+                {
+                    Logger.Error("Файл schedule.xml не найден!");
+                    return;
+                }
+                var content = File.ReadAllText(path);
+                xDoc.LoadXml(content.Replace("windows-1251", "utf-8"));
+                XmlNode xml_groups = xDoc.GetElementsByTagName("timetable").Item(0)!;
+                var total = xml_groups.ChildNodes.Count;
+                int current = 0;
+                using var progress = new ProgressBar();
+
+                foreach (XmlNode xml_group in xml_groups)
+                {
+                    groups.Add(new Group(xml_group.Attributes!["name"]!.Value, GetWeeks(xml_group)));
+                    current++;
+                    progress.Report((double)(100f / total) * current);
+                }
+                Logger.Info($"Расписание успешно загружено за {progress.stopWatch.ElapsedMilliseconds} ms");
             }
-            var content = File.ReadAllText(path);
-            xDoc.LoadXml(content.Replace("windows-1251", "utf-8"));
-            XmlNode groups = xDoc.GetElementsByTagName("timetable").Item(0)!;
-            foreach(var group in groups)
+            catch(Exception ex)
             {
-                Console.WriteLine(group);
+                Logger.Error(ex.ToString());
             }
         }
 
+        protected static List<Week> GetWeeks(XmlNode xml_group)
+        {
+            var weeks = new List<Week>();
+            foreach (XmlNode week in xml_group)
+                weeks.Add(new Week(int.Parse(week.Attributes!["number"]!.Value), GetDays(week)));
+            return weeks;
+        }
+
+        protected static List<Day> GetDays(XmlNode week)
+        {
+            var days = new List<Day>();
+            foreach (XmlNode day in week)
+                days.Add(new Day(day.Attributes!["name"]!.Value, GetLessons(day)));
+            return days;
+        }
+
+        protected static List<Lesson> GetLessons(XmlNode day)
+        {
+            var lessons = new List<Lesson>();
+            foreach (XmlNode lesson in day)
+                lessons.Add(new Lesson(int.Parse(lesson.Attributes!["number"]!.Value), GetSubgroups(lesson)));
+            return lessons;
+        }
+
+        protected static List<Subgroup> GetSubgroups(XmlNode lesson)
+        {
+            var subgroups = new List<Subgroup>();
+            foreach (XmlNode subgroup in lesson)
+                subgroups.Add(new Subgroup(
+                    int.Parse(subgroup.Attributes!["number"]!.Value),
+                    subgroup.Attributes!["subject"]!.Value,
+                    subgroup.Attributes!["short_subject"]!.Value,
+                    subgroup.Attributes!["teacher"]!.Value,
+                    subgroup.Attributes!["cabinet"]!.Value));
+            return subgroups;
+        }
+
+        
     }
 }
