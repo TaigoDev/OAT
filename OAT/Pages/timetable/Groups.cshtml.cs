08be03f8e70fe2b4588c6a758cf59fa9ec9ca6fd
All-exei <aleksandrpehterev26@gmail.com>
2023-07-26 19:57:03 +0600

    add timetable and groups pages + documents page reworked + change accordion scrollbar

---
+++ OAT/Pages/timetable/Groups.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OAT.Pages.timetable
+{
+    public class GroupsModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public GroupsModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ OAT/Pages/timetable/Timetable.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OAT.Pages.timetable
+{
+    public class TimetableModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public TimetableModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
