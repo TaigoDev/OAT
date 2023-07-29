3432e57b227e365afd98a73d47c330f67b875d29
All-exei <aleksandrpehterev26@gmail.com>
2023-07-29 22:37:28 +0600

    add newWorkshops page + add informationCommunicationTechnologies page (beta) + fix visual bugs + another improves

---
+++ OAT/Pages/projectsAndEvents/education/InformationCommunicationTechnologies.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OAT.Pages.projectsAndEvents.education
+{
+    public class InformationCommunicationTechnologiesModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public InformationCommunicationTechnologiesModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
