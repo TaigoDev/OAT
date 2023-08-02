e759cd36205b649d48c20778df22caa1311f8c4a
All-exei <aleksandrpehterev26@gmail.com>
2023-08-02 15:58:22 +0600

    add duck page

---
+++ OAT/Pages/Duck.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OAT.Pages
+{
+    public class DuckModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public DuckModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
