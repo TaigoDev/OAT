0c22abb1e79c78f8a2d22f42870e7602c132d11f
All-exei <aleksandrpehterev26@gmail.com>
2023-08-02 14:25:08 +0600

    add pay page

---
+++ OAT/Pages/feedback/Pay.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OAT.Pages.feedback
+{
+    public class PayModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public PayModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
