22d08ade115c0ef82903c02433287cc498906c00
All-exei <aleksandrpehterev26@gmail.com>
2023-08-06 17:50:24 +0600

    add methodicalActivity page + add teacherAndMentor page + add animation at main page + add links to new pages at main page

---
+++ OAT/Pages/projectsAndEvents/TeacherAndMentor.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OAT.Pages
+{
+    public class TeacherAndMentorModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public TeacherAndMentorModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
