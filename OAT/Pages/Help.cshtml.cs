65e0b8c453f10ff098072663ed0e2f25ba835309
All-exei <aleksandrpehterev26@gmail.com>
2023-09-20 16:45:52 +0600

    main color lighter + change programm document at professionalitet + add help page (alpha)

---
+++ OAT/Pages/Help.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OAT.Pages
+{
+    public class HelpModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public HelpModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
