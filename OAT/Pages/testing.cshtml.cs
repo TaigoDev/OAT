d411be96f02ccc23c1b761faa1c4b79d5c372758
All-exei <aleksandrpehterev26@gmail.com>
2023-10-02 16:42:12 +0600

    add testing of omaviat

---
+++ OAT/Pages/testing.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OAT.Pages
+{
+    public class testingModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public testingModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
