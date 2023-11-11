ef473d6c363eaee29ac357513bd7f8bb34f5b6cc
TaigoStudio <studio@taigo.xyz>
2023-11-11 16:26:33 +0600

    news sort

--- OAT/Readers/NewsReader.cs
+++ OAT/Readers/NewsReader.cs
@@ -15,6 +15,7 @@ public class NewsReader
         using var connection = new MySqlConnection(DataBaseUtils.GetConnectionString());
         var records = await connection.QueryAllAsync<News>();
         news = records.ToList();
+        news.OrderBy(e => DateTime.ParseExact(e.date, "yyyy-MM-dd", null));
         news.Reverse();
         pages = news.PagesSplit(10);
     }
