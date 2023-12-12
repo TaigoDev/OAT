eef01eaf29070e545a19f036b2417558c6478fba
TaigoStudio <studio@taigo.xyz>
2023-12-12 15:55:33 +0600

    Change path

--- OAT/Controllers/ImageController.cs
+++ OAT/Controllers/ImageController.cs
@@ -35,7 +35,7 @@ namespace OAT.Controllers
             if (FullName is null)
                 return Redirect("/images/basic/unnamed.jpg");
 
-            var workedFolder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "people");
+            var workedFolder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images", "people");
             var files = Directory.GetFiles(workedFolder, $"{FullName}.*");
             if(!files.Any())
                 return Redirect("/images/basic/unnamed.jpg");
