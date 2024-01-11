2ab3971bff2dd9677b1307e390fc506256fe08f8
TaigoStudio <studio@taigo.xyz>
2024-01-11 13:10:53 +0600

    Add link to date

--- OAT/Pages/timetable/Changes.cshtml.cs
+++ OAT/Pages/timetable/Changes.cshtml.cs
@@ -13,26 +13,22 @@ namespace OAT.Pages
             _logger = logger;
         }
 
-        public List<Changes> changes { get; set; }
+        public List<Changes>? changes { get; set; }
         public string? sheet { get; set; }
         public int? corpus { get; set; }
 
         public void OnGet(int? corpus, string? sheet)
         {
             if(corpus is null)
-            {
-                HttpContext.Response.Redirect("/timetable/ClassesChanges");
                 return;
-            }
+            
 
             this.corpus = corpus;
             changes = ChangesController.GetListChanges((int)corpus!);
 
             if (!changes.Any())
-            {
-                HttpContext.Response.Redirect("/timetable/ClassesChanges");
                 return;
-            }
+            
             this.sheet = sheet ?? changes.Last().SheetName;
         }
     }
