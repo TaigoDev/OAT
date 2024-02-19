b0bc316a86ae365b5a82383601a82b5dd645f72b
TaigoStudio <studio@taigo.xyz>
2024-02-19 12:52:11 +0600

    Fix bugs

--- OAT/Pages/timetable/Changes.cshtml.cs
+++ OAT/Pages/timetable/Changes.cshtml.cs
@@ -29,22 +29,4 @@ namespace OAT.Pages
 		}
 	}
 
-	public class RowChangesHelper
-	{
-		public static List<string> GetListCSS()
-		{
-			var show_css_class = new List<string>();
-			for (int i = 0; i < new Random().Next(1, 8); i++)
-				show_css_class.Add(StringUtils.RandomString(15, true));
-			return show_css_class;
-		}
-
-		public static List<string> ToCSSList(params List<string>[] strings)
-		{
-			var css = new List<string>();
-			foreach (var s in strings)
-				css.AddRange(s);
-			return css.OrderBy(x => new Random().Next()).ToList();
-		}
-	}
 }
