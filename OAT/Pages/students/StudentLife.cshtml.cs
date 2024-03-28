14c6dfa0ac1b5518d2f6504481e5399c812aef36
Alex <Alex@DESKTOP-A5T3HAD>
2024-03-28 14:04:07 +0600

    added StudentLife page + updated Добрянская Анжела Петровна photo (added)

---
+++ OAT/Pages/students/StudentLife.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OAT.Pages.students
+{
+	public class StudentLifeModel : PageModel
+	{
+		private readonly ILogger<IndexModel> _logger;
+
+		public StudentLifeModel(ILogger<IndexModel> logger)
+		{
+			_logger = logger;
+		}
+
+		public void OnGet()
+		{
+
+		}
+	}
+}
