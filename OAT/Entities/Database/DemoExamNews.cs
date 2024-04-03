feea63e1995bdf3e1459ef146aba9f11f70f586c
TaigoStudio <studio@taigo.xyz>
2024-04-03 15:44:25 +0600

    add demo exam backend

---
+++ OAT/Entities/Database/DemoExamNews.cs
@@ -0,0 +1,42 @@
+﻿using OAT.Utilities;
+
+namespace OAT.Entities.Database
+{
+	[MysqlTable]
+	public class DemoExamNews
+	{
+
+
+		public DemoExamNews(string date, string title, string description, string short_description, List<string> photos)
+		{
+			id = DataBaseUtils.getLastId("DemoExamNews").GetAwaiter().GetResult();
+			this.date = date;
+			this.title = title;
+			this.description = description;
+			this.short_description = short_description;
+			this.photos = photos.toJson();
+		}
+		public DemoExamNews() { }
+
+		public DemoExamNews(int id, string date, string title, string description, string short_description, string photos)
+		{
+			this.id = id;
+			this.date = date;
+			this.title = title;
+			this.description = description;
+			this.short_description = short_description;
+			this.photos = photos;
+		}
+
+		public int id { get; set; }
+		public string date { get; set; }
+		public string title { get; set; }
+		public string description { get; set; }
+		public string short_description { get; set; }
+		public string photos { get; set; }
+
+
+		public List<string> GetPhotos() =>
+			photos.toObject<List<string>>();
+	}
+}
