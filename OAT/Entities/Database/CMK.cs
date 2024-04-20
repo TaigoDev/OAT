f6b112e5a4de64eba90823d717a5ca1a0519c4f9
TaigoStudio <studio@taigo.xyz>
2024-04-20 18:55:56 +0600

    add cmk entity

---
+++ OAT/Entities/Database/CMK.cs
@@ -0,0 +1,10 @@
+﻿namespace OAT.Entities.Database
+{
+	public class CMK
+	{
+
+		public int id { get; set; }
+		public List<string> compositions { get; set; }
+		public string history { get; set; }
+	}
+}
