858b8270ba26f0f16652f4c5af6ddcc2f1bbf335
Alex <Alex@DESKTOP-A5T3HAD>
2024-04-24 15:13:45 +0600

    changed people + added ZOsDT + fixes at contacts page + fixes at CommonIntelligence page + another fixes and improves

---
+++ OAT/Pages/departments/ZOsDT.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OAT.Pages.departments
+{
+	public class ZOsDTModel : PageModel
+	{
+		private readonly ILogger<IndexModel> _logger;
+
+		public ZOsDTModel(ILogger<IndexModel> logger)
+		{
+			_logger = logger;
+		}
+
+		public void OnGet()
+		{
+
+		}
+	}
+}
