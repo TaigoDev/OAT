a78102bc1e4795c3e96356b1370d342b5d639874
Alex <Alex@DESKTOP-A5T3HAD>
2024-05-08 15:52:27 +0600

    added YearOfFamily page + changed content at Logistics page + visual fixed and improved

---
+++ OAT/Pages/projectsAndEvents/YearOfFamily.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OAT.Pages
+{
+	public class YearOfFamilyModel : PageModel
+	{
+		private readonly ILogger<IndexModel> _logger;
+
+		public YearOfFamilyModel(ILogger<IndexModel> logger)
+		{
+			_logger = logger;
+		}
+
+		public void OnGet()
+		{
+
+		}
+	}
+}
