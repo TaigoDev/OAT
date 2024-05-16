7be5d9324faeee4a6963ea98ba30e52afada1932
Alex <Alex@DESKTOP-A5T3HAD>
2024-05-16 19:56:20 +0600

    many bug fixes + added pravila priema document

---
+++ OAT/Pages/departments/TM.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OAT.Pages
+{
+	public class TMModel : PageModel
+	{
+		private readonly ILogger<IndexModel> _logger;
+
+		public TMModel(ILogger<IndexModel> logger)
+		{
+			_logger = logger;
+		}
+
+		public void OnGet()
+		{
+
+		}
+	}
+}
