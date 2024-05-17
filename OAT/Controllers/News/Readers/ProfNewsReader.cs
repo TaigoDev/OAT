d17987af94a7efef13d009f68d66547a9d486bf1
TaigoStudio <126141594+TaigoStudio@users.noreply.github.com>
2024-05-17 18:06:33 +0600

    Fix bugs

--- OAT/Controllers/News/Readers/ProfNewsReader.cs
+++ OAT/Controllers/News/Readers/ProfNewsReader.cs
@@ -11,7 +11,7 @@ namespace OAT.Controllers.MNews.Readers
 		public static async Task init()
 		{
 			using var connection = new DatabaseContext();
-			var records = await connection.ProfNews.ToListAsync();
+			news = await connection.ProfNews.ToListAsync();
 			news = news.OrderByDescending(e => DateTime.ParseExact(e.date, "yyyy-MM-dd", null)).ToList();
 			news = [.. news.Where(e => e.IsFixed), .. news.Where(e => !e.IsFixed)]; 
 			pages = news.PagesSplit(10);
