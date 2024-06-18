1fdf5cff865643ce869a0058cda2eb1829f3f104
TaigoStudio <126141594+TaigoStudio@users.noreply.github.com>
2024-06-18 13:18:41 +0600

    remove logger

--- OAT/Program.cs
+++ OAT/Program.cs
@@ -10,9 +10,6 @@ using OAT.Utilities;
 using OAT.Utilities.Telegram;
 using OfficeOpenXml;
 using RepoDb;
-List<string> s = ["ПОИТ", "КTЗС", "ТехРЭМ", "ТехСвар", "ТехАРС", "ЭКУП", "СГД", "ГД", "ЕНД", "ТехОПИТ", "ФКД", "ТехМаш", "ТехРЭО", "Ин.яз", "Информатики", "Математики", "РусЯз"];
-foreach (var item in s)
-	Console.WriteLine(item + " - " + Translit.TranslitRusToEn(item));
 await Configurator.init();
 ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
 var builder = WebApplication.CreateBuilder(args);
