ac7c5255f63c040b931d8ff9961f2894fdfa8535
TaigoStudio <126141594+TaigoStudio@users.noreply.github.com>
2024-06-27 14:48:33 +0600

    update blazor

--- OAT/Program.cs
+++ OAT/Program.cs
@@ -18,7 +18,6 @@ await TelegramBot.init();
 DownDetector.init();
 await DatabaseHelper.WaitStableConnection();
 using var db = new DatabaseContext();
-Logger.Info("Test");
 Console.WriteLine($"Количество новостей: {db.News.Count()} ");
 
 //db.Database.Migrate();
