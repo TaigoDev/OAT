da30bb065255ea252181f06144ca4581726d3ff5
Alex <Alex@DESKTOP-A5T3HAD>
2024-07-19 19:21:22 +0600

    added beta map page

---
+++ OAT/Pages/feedback/Map.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OAT.Pages.feedback
+{
+	public class MapModel : PageModel
+	{
+		private readonly ILogger<IndexModel> _logger;
+
+		public MapModel(ILogger<IndexModel> logger)
+		{
+			_logger = logger;
+		}
+
+		public void OnGet()
+		{
+
+		}
+	}
+}
