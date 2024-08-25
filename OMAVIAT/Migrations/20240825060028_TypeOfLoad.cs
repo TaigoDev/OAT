c3f632ddfc7fc96c2150bf8e6039f1ebd6a1d001
TaigoStudio <126141594+TaigoStudio@users.noreply.github.com>
2024-08-25 12:02:15 +0600

    files

---
+++ OMAVIAT/Migrations/20240825060028_TypeOfLoad.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OMAVIAT.Migrations
+{
+    /// <inheritdoc />
+    public partial class TypeOfLoad : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TypeOfLoad",
+                table: "AcademicLoads",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TypeOfLoad",
+                table: "AcademicLoads");
+        }
+    }
+}
