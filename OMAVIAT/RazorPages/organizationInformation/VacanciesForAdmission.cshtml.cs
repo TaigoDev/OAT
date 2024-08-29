8cad97c91511b3309c43deca31d02132b53d9870
Alex <Alex@DESKTOP-A5T3HAD>
2024-08-29 18:35:00 +0600

    added photos of personalStaff + changed licenzia document + changed link to Jobs page (VacanciesForAdmission page)

---
+++ OMAVIAT/RazorPages/organizationInformation/VacanciesForAdmission.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OMAVIAT.Pages.organizationInformation
+{
+	public class VacanciesForAdmissionModel : PageModel
+	{
+		private readonly ILogger<VacanciesForAdmissionModel> _logger;
+
+		public VacanciesForAdmissionModel(ILogger<VacanciesForAdmissionModel> logger)
+		{
+			_logger = logger;
+		}
+
+		public void OnGet()
+		{
+
+		}
+	}
+}
