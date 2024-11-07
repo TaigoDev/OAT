2182cf71a886e15a155a749b9b5fd0c2df034d89
Alex <Alex@DESKTOP-A5T3HAD>
2024-11-07 17:13:43 +0600

    added TargetedTraining page

---
+++ OMAVIAT/RazorPages/students/TargetedTraining.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OMAVIAT.Pages.students
+{
+	public class TargetedTrainingModel : PageModel
+	{
+		private readonly ILogger<TargetedTrainingModel> _logger;
+
+		public TargetedTrainingModel(ILogger<TargetedTrainingModel> logger)
+		{
+			_logger = logger;
+		}
+
+		public void OnGet()
+		{
+
+		}
+	}
+}
