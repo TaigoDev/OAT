0cea4cfa7d866e1d06ff2770a7e021e2ab421da3
Alex <Alex@DESKTOP-A5T3HAD>
2024-12-22 15:19:38 +0600

    changed content at professionals pages

---
+++ OMAVIAT/RazorPages/projectsAndEvents/professionals/Documents.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OMAVIAT.Pages.projectsAndEvents.professionals {
+	public class DocumentsModel : PageModel {
+		private readonly ILogger<DocumentsModel> _logger;
+
+		public DocumentsModel(ILogger<DocumentsModel> logger)
+		{
+			_logger = logger;
+		}
+
+		public void OnGet()
+		{
+
+		}
+	}
+}
