a0b6dbebe05d7398966b2f8c7e619e4605c4a054
TaigoStudio <126141594+TaigoStudio@users.noreply.github.com>
2025-02-05 20:49:43 +0600

    add mail service

---
+++ OMAVIAT/Entities/Configs/MailConfig.cs
@@ -0,0 +1,13 @@
+﻿namespace OMAVIAT.Entities.Configs;
+
+public class MailConfig
+{
+	public string SmtpServer { get; set; } = "smtp.yandex.ru";
+	public int SmtpPort { get; set; } = 465;
+	public bool EnableSsl { get; set; } = true;
+	public string EmailUser { get; set; } = "dod@oat.ru";
+	public string EmailPassword { get; set; } = "Pa$swOrD";
+	public string SendTo { get; set; } = "dod@oat.ru";
+	public bool EnableProxy { get; set; } = true;
+
+}
