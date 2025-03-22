cefad6c0ed6c0463cdcb6745f1b37b10f0667b90
oneofthegreatest <aleksandrpehterev26@gmail.com>
2025-03-22 12:20:54 +0600

    added YearOfVictory page

---
+++ OMAVIAT/RazorPages/projectsAndEvents/YearOfVictory.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OMAVIAT.Pages;
+
+public class YearOfVictoryModel : PageModel
+{
+	private readonly ILogger<YearOfVictoryModel> _logger;
+
+	public YearOfVictoryModel(ILogger<YearOfVictoryModel> logger)
+	{
+		_logger = logger;
+	}
+
+	public void OnGet()
+	{
+	}
+}
