723d5476e23f2a7b46106218fc47db09a2b76827
oneofthegreatest <aleksandrpehterev26@gmail.com>
2025-04-07 23:37:36 +0600

    added distanceLearning pages + changed footer + changed content at Specialities page (professionalitet)

---
+++ OMAVIAT/RazorPages/another/distanceLearning/Main.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OMAVIAT.Pages.distanceLearning.Main;
+
+public class MainModel : PageModel
+{
+	private readonly ILogger<MainModel> _logger;
+
+	public MainModel(ILogger<MainModel> logger)
+	{
+		_logger = logger;
+	}
+
+	public void OnGet()
+	{
+	}
+}
---
+++ OMAVIAT/RazorPages/another/distanceLearning/Specialities.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OMAVIAT.Pages.distanceLearning.Specialities;
+
+public class SpecialitiesModel : PageModel
+{
+	private readonly ILogger<SpecialitiesModel> _logger;
+
+	public SpecialitiesModel(ILogger<SpecialitiesModel> logger)
+	{
+		_logger = logger;
+	}
+
+	public void OnGet()
+	{
+	}
+}
