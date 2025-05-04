2e1fef8439c3380fbcfcb96ce3aa502384120750
oneofthegreatest <aleksandrpehterev26@gmail.com>
2025-05-04 20:46:13 +0600

    added YearOfSPO page + changed content at QuestionAnswer, SelectionCommittee, CareerGuidanceEvents pages

---
+++ OMAVIAT/RazorPages/projectsAndEvents/YearOfSPO.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OMAVIAT.Pages;
+
+public class YearOfSPOModel : PageModel
+{
+	private readonly ILogger<YearOfSPOModel> _logger;
+
+	public YearOfSPOModel(ILogger<YearOfSPOModel> logger)
+	{
+		_logger = logger;
+	}
+
+	public void OnGet()
+	{
+	}
+}
