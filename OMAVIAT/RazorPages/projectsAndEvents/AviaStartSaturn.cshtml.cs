0d255b6f30c1b0fd1ab26301a7c8cbf2ada5cf81
oneofthegreatest <aleksandrpehterev26@gmail.com>
2025-05-14 18:39:29 +0600

    added AviaStartSaturn page + changed some another pages + chenged odkSaturn logo (partners)

---
+++ OMAVIAT/RazorPages/projectsAndEvents/AviaStartSaturn.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OMAVIAT.Pages.projectsAndEvents;
+
+public class AviaStartSaturnModel : PageModel
+{
+	private readonly ILogger<AviaStartSaturnModel> _logger;
+
+	public AviaStartSaturnModel(ILogger<AviaStartSaturnModel> logger)
+	{
+		_logger = logger;
+	}
+
+	public void OnGet()
+	{
+	}
+}
