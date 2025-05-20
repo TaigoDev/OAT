0696218cf1f8dfe6399b2075de9fb9276cb7ba35
oneofthegreatest <aleksandrpehterev26@gmail.com>
2025-05-21 00:24:22 +0600

    changed content at Contacts, YearOfSPO pages

---
+++ OMAVIAT/RazorPages/another/VeteransSPO.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OMAVIAT.Pages.another;
+
+public class VeteransSPOModel : PageModel
+{
+	private readonly ILogger<VeteransSPOModel> _logger;
+
+	public VeteransSPOModel(ILogger<VeteransSPOModel> logger)
+	{
+		_logger = logger;
+	}
+
+	public void OnGet()
+	{
+	}
+}
